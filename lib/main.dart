70607c9d4670f06d6ab6780cde10f46f3cc47172
kimilguk <kimilguk19@gmail.com>
2026-03-23 13:19:23 +0900

    11주차 02교시 강의 소스1

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter_markdown/flutter_markdown.dart'; // 마크다운 렌더�
 
 void main() async {
   // .env 파일에서 환경(environment) 변수를 로드합니다
-  await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: 'assets/.env');
   runApp(const MyApp());
 }
 
